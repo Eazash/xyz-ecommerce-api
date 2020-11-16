5de50f91800eb7009475cd4b9a987a27feb4cc65
Ezra Ashenafi <eazash22@gmail.com>
2020-11-17 02:09:29 +0300

    setup swagger and swagger-ui

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const DB_URL = 'mongodb://localhost:27017/xyz';
+const YAML = require('yamljs');
+
+const swagger_options = YAML.load('./swagger.yml');
+const swaggerUi = require('swagger-ui-express');
 
 mongoose.connect(DB_URL, {
   useCreateIndex: true,
@@ -15,5 +19,7 @@ mongoose.connect(DB_URL, {
   console.log(error);
   process.exit(1);
 })
+
+app.use('/api_docs', swaggerUi.serve, swaggerUi.setup(swagger_options));
 
 app.listen(PORT, () => console.log(`API server running at ${PORT}`));
